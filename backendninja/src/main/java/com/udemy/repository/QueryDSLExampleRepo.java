bba7b0303f3fb8539642afcbe02b8ee4d3e07949
Mario Ortega <mcor63@gmail.com>
2018-02-03 20:21:34 -0400

    Se finaliza practica QueryDSL y se agrega rama para respaldar todas las practicas hechas antes de limpiar el proyecto y hacer un proyecto completo

--- backendninja/src/main/java/com/udemy/repository/QueryDSLExampleRepo.java
+++ backendninja/src/main/java/com/udemy/repository/QueryDSLExampleRepo.java
@@ -7,6 +7,8 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.udemy.entity.Course;
 import com.udemy.entity.QCourse;
@@ -19,12 +21,21 @@ public class QueryDSLExampleRepo {
 	@PersistenceContext
 	private EntityManager em;
 
-	public void find() {
-		JPAQuery<Course> query = new JPAQuery<Course>(em);
+	public Course find(boolean exist) {
+		JPAQuery<Course> query = new JPAQuery<>(em);
 
-		Course course1 = query.select(qCourse).from(qCourse).where(qCourse.id.eq(23)).fetchOne();
+		BooleanBuilder predicateBuilder = new BooleanBuilder(qCourse.description.endsWith("OP"));
 
-		List<Course> courses = query.select(qCourse).from(qCourse).where(qCourse.hours.between(20, 50)).fetch();
+		if (exist) {
+			Predicate predicate2 = qCourse.id.eq(23);
+			predicateBuilder.and(predicate2);
+		} else {
+			Predicate predicate3 = qCourse.name.endsWith("OP");
+			predicateBuilder.or(predicate3);
+		}
+
+		return query.select(qCourse).from(qCourse).where(predicateBuilder).fetchOne();
+
 	}
 
 }
